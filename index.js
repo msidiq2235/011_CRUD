ac6a25285ec27145790319ce8f8cd9f455839957
msidiq2235 <muhsidiq2005@gmail.com>
2025-10-20 12:32:57 +0700

    Menambahkan route API untuk mengambil data dari database

--- index.js
+++ index.js
@@ -28,3 +28,15 @@ db.connect((err) => {
     }
     console.log('Connection Successfully');
 });
+
+app.get('/api/users', (req, res) => {
+    db.query('SELECT * FROM users', (err, results) => {
+        if (err) {
+            console.error('Error executing query: ' + err.stack);
+            res.status(500).send('Error fetching users');
+            return;
+        }
+
+        res.json(results);
+    });
+});
